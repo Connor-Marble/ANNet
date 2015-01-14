e37f6bcc62e449618562ff802dbd1bd90d420bf8
Connor-Marble <MarbleConnor@gmail.com>
2015-01-14 11:47:10 -0500

    backpropogate function should find error for every neuron in the network now

--- NeuralNetCore/NeuralNet.py
+++ NeuralNetCore/NeuralNet.py
@@ -12,9 +12,9 @@ class NeuralNetwork():
         layer = []
         for i in range(size):
             if len(self.layers) > 0:
-                layer.append(Neuron(self.layers[len(self.layers)-1],0))
+                layer.append(Neuron(self.layers[len(self.layers)-1],uniform(-0.5,0.5)))
             else:
-                layer.append(Neuron([],0))
+                layer.append(Neuron([],uniform(-0.5,0.5)))
 
         self.layers.append(layer)
 
@@ -37,7 +37,13 @@ class NeuralNetwork():
                 neuron.evaluate()
 
     def backpropogate(self, targetoutput):
-        error = self.get_output_error(targetoutput)
+        error = []
+        error.append(self.get_output_error(targetoutput))
+        for layer in reversed(self.layers[1:-1]):
+            newerrorlayer = [neuron.geterror(error[0]) for neuron in layer]
+            error.insert(0, newerrorlayer)
+        print error
+        
         
     def get_output_error(self, targetoutput):
         outputvalues = [x.output for x in self.layers[-1]]
@@ -47,7 +53,9 @@ if __name__=='__main__':
     net = NeuralNetwork([3,5,6,5,4])
     net.setinput([0,0.9,0.1])
     net.evaluate()
+    print net
+    print 'error:'
     net.backpropogate(range(4))
-    print(net)
+    
                     
                 
